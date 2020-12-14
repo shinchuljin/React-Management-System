d1fcba67e6a7337cc5d492c19877d6db7d13961a
charlie shin <shinchuljin@hakjisa.co.kr>
2020-12-14 09:20:51 +0900

    컴포넌트 내용분리하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <Customer/>
-  );
+
+const customers = [
+{
+  'id':1,
+  'image': 'http://placeimg.com/64/64/any',
+  'name' : 'Charlie Shin',
+  'birthday' : '991004',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '나동빈',
+  'birthday': '960508',
+  'gender': '남자',
+  'job': '프로그래머'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '이순신',
+  'birthday': '961127',
+  'gender': '남자',
+  'job': '디자이너'
+}
+
+]
+class App extends Component{
+  render(){
+    return (
+      <div>
+
+        <Customer
+          id = {customers[0].id}
+          image = {customers[0].image}
+          name = {customers[0].name}
+          birthday = {customers[0].birthday}
+          gender = {customers[0].gender}
+          job = {customers[0].job}
+        />
+        <Customer
+          id={customers[1].id}
+          image={customers[1].image}
+          name={customers[1].name}
+          birthday={customers[1].birthday}
+          gender={customers[1].gender}
+          job={customers[1].job}
+        />
+        <Customer
+          id={customers[2].id}
+          image={customers[2].image}
+          name={customers[2].name}
+          birthday={customers[2].birthday}
+          gender={customers[2].gender}
+          job={customers[2].job}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
